b55612c2c6709b0c621f4e43a5541f0333815e6f
WebMats <lorlando127@gmail.com>
2019-05-31 12:14:41 -0400

    finished cityreader.py: csv reader for class instantiation.

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -14,13 +14,24 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+import csv
+
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
 cities = []
 
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+  with open('src/cityreader/cities.csv', 'r') as cityF:
+    cityR = csv.reader(cityF)
+    for row in cityR:
+      cities.append(City(row[0], row[3], row[4]))
     return cities
 
 cityreader(cities)
